1578acfa94b3e3dd438d2d94e109d0aa903b7b0e
CYBXR <cybxr@CYBXRs-MacBook-Pro.local>
2023-11-19 14:52:20 +0000

    Added docs strings and API related files

---
+++ RickAndMorty/APIClient/RMEndpoint.swift
@@ -0,0 +1,17 @@
+//
+//  RMEndpoint.swift
+//  RickAndMorty
+//
+//  Created by CYBXR on 19/11/2023.
+//
+
+import Foundation
+
+
+/// Represents unique API endpoint
+@frozen enum RMEndpoint: String {
+    
+    case character
+    case location
+    case episode
+}
---
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,14 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by CYBXR on 19/11/2023.
+//
+
+import Foundation
+
+
+/// Object that represents a single API call
+final class RMRequest {
+    
+}
